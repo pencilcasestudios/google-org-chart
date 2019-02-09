713fcfb2ddba094e9ec9efd10acc709b6f73aeb6
Silumesii Maboshe <silumesii@bongohive.co.zm>
2019-02-09 13:12:28 +0200

    Use HTTPS to link to the Google org chart API.

--- google-org-chart.php
+++ google-org-chart.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Google Org Chart
 Description: Shortcode plugin for simpler generation of org charts using Google's org chart API.
-Version: 1.0.1
+Version: 1.0.2
 Author: Aleksandar Arsovski
 License: GPL2
 */
@@ -10,7 +10,7 @@ License: GPL2
 /*  Copyright 2011  Aleksandar Arsovski  (email : alek_ars@hotmail.com)
 
     This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License, version 2, as 
+    it under the terms of the GNU General Public License, version 2, as
     published by the Free Software Foundation.
 
     This program is distributed in the hope that it will be useful,
@@ -38,28 +38,28 @@ add_shortcode( 'org_chart_item', 'google_org_chart_item_shortcode' );
 
 // Google Org Chart callback
 function google_org_chart_shortcode( $attr, $content ) {
-	
+
 	// Globalize the org chart nodes array
 	global $goc_nodes;
-	
-	
+
+
 	// Attributes: default width and height are 300px
 	extract( shortcode_atts( array(
 		'width' => '300',
 		'height' => '300',
 	), $attr ) );
-	
-	
+
+
 	// Entering collapsible as a no-value attribute will set the each parent node in the org chart as collapsible
 	if( in_array( "collapsible", $attr ) )
 		$collapsible = 'true';
 	else
 		$collapsible = 'false';
-	
-	
+
+
 	// Starting off the return string with the set up needed before nodes are introduces
 	$return_string = "
-	<script type='text/javascript' src='http://www.google.com/jsapi'></script>
+	<script type='text/javascript' src='https://www.google.com/jsapi'></script>
 	<script type='text/javascript'>
     		google.load('visualization', '1', {packages: ['orgchart']});
     	</script>
@@ -71,28 +71,28 @@ function google_org_chart_shortcode( $attr, $content ) {
 				data.addColumn('string', 'Parent');
         		data.addColumn('string', 'HoverOver');
         		";
-    
-    
-    // Execute all the individual node shortcodes contained within the org chart shortcode block
+
+
+  // Execute all the individual node shortcodes contained within the org chart shortcode block
 	do_shortcode( $content );
-	
-	
+
+
 	// Get the total number of nodes
 	$chart_node_count = count( $goc_nodes );
-	
-	
+
+
 	// Add the number of nodes to the return screen for further set up purposes
 	$return_string .= "data.addRows(" . $chart_node_count . ");";
-	
-	
+
+
 	// Set up the all the nodes and their attributes
 	foreach( $goc_nodes as $node_id => $attributes ) {
-		
+
 		$title = $attributes[ 'title' ];
 		$content = $attributes[ 'content' ];
 		$parent = $attributes[ 'parent' ];
 		$hover = $attributes[ 'hover' ];
-		
+
 		// Set up the title and the content attributes of each node
 		// If title and content are both provided, title acts more as an ID
 		// Otherwise the title acts as the ID and content
@@ -104,40 +104,40 @@ function google_org_chart_shortcode( $attr, $content ) {
     			$return_string .= "data.setCell(".$node_id.", 0, '".$title."');
     			";
     	}
-    	
+
     	// Set up the nodes parent if it has one otherwise leave the node at top level
     	if ( isset( $parent ) )
     		$return_string .= "data.setCell(".$node_id.", 1, '".$parent."');
     		";
-    	
+
     	// Set up the information displayed upon hovering over the node
     	if ( isset( $hover ) )
     		$return_string .= "data.setCell(".$node_id.", 2, '".$hover."');
     		";
     }
-    
-    
+
+
     // Finish off the return string with the collpsible, width, and height settings
 	$return_string .= '
-		// Create and draw the visualization.
+	// Create and draw the visualization.
         	new google.visualization.OrgChart(document.getElementById("visualization")).
             	draw(data, {allowHtml: true, allowCollapse: '.$collapsible.'});
       		}
 
 		google.setOnLoadCallback(drawVisualization);
     	</script>
-    	
+
     	<div id="visualization" style="width: '.$width.'px; height: '.$height.'px;"></div>';
- 
+
  	return $return_string;
 }
 
 
 // Google Org Chart Node callback
 function google_org_chart_item_shortcode( $attr ) {
-	
+
 	global $goc_nodes;
-	
+
 	// Add the node's attributes to the node array
 	$goc_nodes[] = array(
 						'title' => isset( $attr['title'] ) ? $attr['title'] : NULL,
@@ -145,5 +145,4 @@ function google_org_chart_item_shortcode( $attr ) {
 						'parent' => isset( $attr['parent'] ) ? $attr['parent'] : NULL,
 						'hover' => isset( $attr['hover'] ) ? $attr['hover'] : NULL
 					);
-					
 }
